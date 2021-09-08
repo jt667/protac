08c3b77e597831f16e397c36fc2618f4cfcf3213
jt667 <55281419+jt667@users.noreply.github.com>
2021-09-08 11:29:47 +0100

    Update protac_classification_quantum_verification602020.py

--- quantum_classification/protac_classification_quantum_verification602020.py
+++ quantum_classification/protac_classification_quantum_verification602020.py
@@ -1,6 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, accuracy_score
 import optimisation_functions
+import pennylane as qml
 
 ##
 ## Prepare the data and split into sets 60/20/20
@@ -15,14 +16,39 @@ x_test_select, x_test_final, y_test_select, y_test_final = train_test_split(
 )
 
 
+# Number of mini batches
+mini_batch_num = 20
+
+
 # File containing hyperparameters
-param_path = "hyperparameters//602020best_params1_trials.txt"
+param_path = "hyperparameters//Circuit2Params.txt"
 
 
 # Load model
 hyperparams = optimisation_functions.hyperparameters()
 hyperparams.load_params(param_path)
 
+# If the model has not been fully trained - train it
+if not hasattr(hyperparams, "final_vars"):
+    var = hyperparams.starting_var
+    opt = qml.AdamOptimizer(
+        stepsize=hyperparams.step_size, beta1=hyperparams.beta1, beta2=hyperparams.beta2
+    )
+
+    batch_size = int(x_train.shape[0] / mini_batch_num)
+
+    for it in range(mini_batch_num):
+        # Update the weights by one optimizer step
+
+        x_train_batch = x_train[it * batch_size : (it + 1) * batch_size]
+        y_train_batch = y_train[it * batch_size : (it + 1) * batch_size]
+        var = opt.step(
+            lambda v: optimisation_functions.cost(
+                v, x_train_batch, y_train_batch, hyperparams.circuit_num
+            ),
+            var,
+        )
+    hyperparams.save_final_vars(var)
 # Compute predictions and scores
 pred_vals = [
     round(
