f06c691ed1903fb8bc1848a78ad374ee7c92fee5
jt667 <55281419+jt667@users.noreply.github.com>
2021-10-08 12:10:14 +0100

    Add files via upload

---
+++ qsharp_implementation/protac_classification_quantum_simple_verification.py
@@ -0,0 +1,44 @@
+from sklearn.metrics import f1_score, accuracy_score
+import optimisation_functions
+import numpy as np
+
+
+def classifier(var,x):
+    measurements = circuit15(var, x)
+
+# Put outputs into a linear model with sigmoid activation function
+    lin_model = np.inner(measurements, var[-11:-1]) + var[-1]
+    sig = 1 / (1 + np.exp((-1) * lin_model))
+    
+    return sig
+
+
+
+##
+## Prepare the data and split into sets 60/20/20
+##
+
+# File containing hyperparameters
+param_path = "hyperparameters//8020best_params40_trials.json"
+verification_model = optimisation_functions.load_params_json(param_path)
+
+x,y = optimisation_functions.data_prep(splits=[0.6,0.2])
+x_train, x_test_select, x_test_final = x
+y_train, y_test_select, y_test_final = y
+
+
+
+# Compute predictions and scores
+pred_vals = [
+    round(
+        optimisation_functions.classifier(
+            verification_model.final_vars, f
+            )
+    )
+    for f in x_test_final
+]
+
+final_score = f1_score(y_test_final, pred_vals)
+final_acc = accuracy_score(y_test_final, pred_vals)
+print("f1 Score: " + str(final_score))
+print("Accuracy: " + str(final_acc))
